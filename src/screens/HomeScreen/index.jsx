ce17e34a3ea29f39182797ae4084499b46a9d745
MyAnIfy <ianyam@efficion.org>
2020-09-02 13:46:45 +0100

    homescreen

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function index() {
     return (
         <View>
-            <Text>Welcome home</Text>
+            <Text>Welcome homescreen</Text>
         </View>
     )
 }
