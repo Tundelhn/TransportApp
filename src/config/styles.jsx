1a522297db3c3efa22d5b899646257cd455f26b1
Tundelhn <tunde.lhn@gmail.com>
2020-09-04 13:21:29 +0100

    encapsulating styles

--- src/config/styles.jsx
+++ src/config/styles.jsx
@@ -3,9 +3,15 @@ import {
   StyleSheet,
   Text,
   View,
-  Platform
+  Platform,
+  Dimensions
 } from "react-native";
 import Constants from "expo-constants"
+
+const {
+  height
+} = Dimensions.get("screen");
+const height_logo = height * 0.28;
 
 const styles = StyleSheet.create({
   text: {
@@ -21,6 +27,134 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginRight: 10,
   },
+
+  footer: {
+    position: "absolute",
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    height: 530,
+    left: 0,
+    top: 282,
+    width: 375,
+
+    // paddingVertical: 50,
+    // paddingHorizontal: 30,
+  },
+  image: {
+    position: "absolute",
+    width: 998,
+    height: 700,
+    left: -309,
+    top: 10,
+  },
+  headerText: {
+    position: "absolute",
+    width: 335,
+    height: 92,
+    top: 92,
+    left: 320,
+  },
+  signupText: {
+    fontSize: 36,
+    lineHeight: 42,
+    color: "#fff"
+  },
+  textCaption: {
+    position: "absolute",
+    width: 335,
+    height: 40,
+    left: 320,
+    top: 197,
+  },
+  textCaptionTwo: {
+    fontSize: 16,
+    lineHeight: 20,
+    color: "#fff"
+  },
+  footerContainer: {
+    position: "absolute",
+    width: 59,
+    height: 23,
+    left: 34,
+    top: 25,
+  },
+  footerContainerText: {
+    fontSize: 18,
+    lineHeight: 21,
+    color: "#000",
+    fontWeight: "500",
+  },
+  footerContainerTextCaption: {
+    position: "absolute",
+    width: 184,
+    height: 28,
+    left: 34,
+    top: 80,
+  },
+  footerContainerSignupText: {
+    fontSize: 16,
+    lineHeight: 17,
+    color: "#000",
+    fontWeight: "bold",
+  },
+  footerContainerButton: {
+    position: "absolute",
+    marginTop: 10,
+    width: "100%",
+    top: 90,
+    left: 15,
+    right: 10,
+  },
+  footerContainerButtonText: {
+    position: "absolute",
+    width: 268,
+    height: 38,
+    left: 20,
+    top: 155,
+  },
+  footerunderText: {
+
+    lineHeight: 18,
+    fontSize: 14
+
+  },
+  footerBottomContainer: {
+    position: "absolute",
+    width: 268,
+    height: 38,
+    left: 20,
+    top: 220,
+  },
+  footerBottonText1: {
+    lineHeight: 18,
+    fontSize: 14,
+    fontWeight: "bold"
+  },
+  footerBottomtext2: {
+    lineHeight: 18,
+    fontSize: 14
+  },
+  driverSignupContainer: {
+    position: "absolute",
+    marginTop: 10,
+    width: "100%",
+    top: 250,
+    left: 15,
+    right: 10,
+  },
+  driverContainerText: {
+    position: "absolute",
+    width: 3008,
+    height: 38,
+    left: 20,
+    top: 310,
+  },
+  driversignupText: {
+    lineHeight: 18,
+    fontSize: 14,
+    color: "#828282"
+  }
 });
 
 export default styles;
--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,11 +1,76 @@
 import React from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
-import TextInput from "../components/AppTextInput";
-
-export default function SignupScreen() {
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  ImageBackground,
+} from "react-native";
+import AppButton from "../components/AppButton";
+import Styles from "../config/styles";
+export default function SignupScreen({ color = "primary" }) {
   return (
-    <View style={styles.container}>
-      <View style={styles.footer}></View>
+    <View>
+      <ImageBackground
+        source={require("../../assets/drivers-bg.jpg")}
+        style={Styles.image}
+      >
+        <View style={Styles.headerText}>
+          <Text style={Styles.signupText}>Commute Easily</Text>
+          <Text style={Styles.signupText}>with "our brand"</Text>
+        </View>
+        <View style={Styles.textCaption}>
+          <Text style={Styles.textCaptionTwo}>
+            Earn good money with your vehicle
+          </Text>
+          <Text style={Styles.textCaptionTwo}>
+            offer ride to fello commuters and
+          </Text>
+        </View>
+      </ImageBackground>
+      <View style={Styles.footer}>
+        <View style={Styles.footerContainer}>
+          <Text style={Styles.footerContainerText}>Signup</Text>
+        </View>
+        <View style={Styles.footerContainerTextCaption}>
+          <Text style={Styles.footerContainerSignupText}>
+            Sign up as regular user
+          </Text>
+        </View>
+        <View style={Styles.footerContainerButton}>
+          <AppButton title="sign Up" color="button" />
+        </View>
+        <View style={Styles.footerContainerButtonText}>
+          <Text style={Styles.footerunderText}>
+            Regular users are able to book rides offer
+          </Text>
+          <Text style={Styles.footerunderText}>Interstate bookings</Text>
+        </View>
+        <View style={Styles.footerBottomContainer}>
+          <Text style={Styles.footerBottonText1}>
+            Do you own a car? sign up here to earn
+          </Text>
+          <Text style={Styles.footerBottomtext2}> wih our brand</Text>
+        </View>
+        <View style={Styles.driverSignupContainer}>
+          <AppButton title="sign Up" color="button" />
+        </View>
+        <View style={Styles.driverContainerText}>
+          <Text style={Styles.driversignupText}>
+            You will be able to offer ride and earn cash
+          </Text>
+          <Text style={Styles.driversignupText}>
+            when you offer ride to other commuters
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
